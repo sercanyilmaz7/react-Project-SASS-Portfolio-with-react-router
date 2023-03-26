727e183ffb6193a88fb9df75cca1669626cd9a61
sercanyilmaz7 <srcn.yilmaz333@gmail.com>
2023-03-27 00:38:38 +0300

    router

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,5 +1,5 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router/router";
+// import { RouterProvider } from "react-router-dom";
+// import { router } from "./router/router";
 import "./App.scss"
 import AppRouter from "./router/AppRouter";
 function App() {
